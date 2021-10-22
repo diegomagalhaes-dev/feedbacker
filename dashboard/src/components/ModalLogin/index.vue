b13657371f3f7040f6a91426d72f3967f76b1c25
diegomagalhaes-dev <digengup@gmail.com>
2021-10-21 23:50:01 -0300

    :zap: Login Modal

--- dashboard/src/components/ModalLogin/index.vue
+++ dashboard/src/components/ModalLogin/index.vue
@@ -1,9 +1,91 @@
 <template>
   <div class="flex justify-between">
     <h1 class="text-4xl font-black text-gray-800">Entre na sua conta</h1>
-    <button class="text-4xl text-gray-600 focus:outline-none">&times;</button>
+    <button @click="close" class="text-4xl text-gray-600 focus:outline-none">
+      &times;
+    </button>
   </div>
-  <div class="mt-16"></div>
+  <div class="mt-16">
+    <!-- este '.prevent' é um modificador do Vue que faz a estratégia de impedir o comportamento dafault dos forms (de recarregar a página ao submeter) -->
+    <form @submit.prevent="handleSubmit">
+      <label class="block" for="">
+        <span class="text-lg font-medium text-gray-800">E-mail</span>
+        <input
+          v-model="state.email.value"
+          type="email"
+          :class="{
+            'border-brand-danger': !!state.email.errorMessage
+          }"
+          class="
+            block
+            w-full
+            px-4
+            py-3
+            mt-1
+            text-lg
+            bg-gray-100
+            border-2 border-transparent
+            rounded
+          "
+          placeholder="example: john@gmail.com"
+        />
+        <span
+          v-if="!!state.email.errorMessage"
+          class="block font-medium text-brand-danger"
+        >
+          {{ state.email.errorMessage }}
+        </span>
+      </label>
+      <label class="block" for="">
+        <span class="text-lg font-medium text-gray-800">Senha</span>
+        <input
+          v-model="state.password.value"
+          type="password"
+          :class="{
+            'border-brand-danger': !!state.password.errorMessage
+          }"
+          class="
+            block
+            w-full
+            px-4
+            py-3
+            mt-1
+            text-lg
+            bg-gray-100
+            border-2 border-transparent
+            rounded
+          "
+        />
+        <span
+          v-if="!!state.password.errorMessage"
+          class="block font-medium text-brand-danger"
+        >
+          {{ state.password.errorMessage }}
+        </span>
+      </label>
+      <button
+        :disable="state.isLoading"
+        type="submit"
+        :class="{
+          'opacity-50': state.isLoading
+        }"
+        class="
+          px-8
+          py-3
+          mt-10
+          text-2xl
+          font-bold
+          text-white
+          rounded-full
+          bg-brand-main
+          focus:outline-none
+          transition:
+          all
+          duration-150
+        "
+      ></button>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -27,9 +109,8 @@ export default {
       }
     });
 
-    function handleSubmite{
+    function handleSubmit() {}
 
-    }
     return {
       state,
       close: modal.close
