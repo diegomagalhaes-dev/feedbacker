9963dc6af87bc270c8bdec15e3e0ba94476d20bf
diegomagalhaes-dev <digengup@gmail.com>
2021-11-16 21:02:03 -0300

    :sparkles: create watch and authenticated routes

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -4,20 +4,26 @@
 </template>
 
 <script>
-import ModalFactory from './components/ModalFactory';
-
 import { watch } from 'vue';
 import { useRouter, useRoute } from 'vue-router';
+
+import ModalFactory from './components/ModalFactory';
 import services from './services';
+import { setCurrentUser } from './store/user';
 
 export default {
   components: {
     ModalFactory
   },
   setup() {
+    // Cuida do roteamento (ações)
     const router = useRouter();
+    // Nos fornece informações da rota
     const route = useRoute();
 
+    // O tratamento de rotas está aqui na App.vue, pois ele é o component principal da aplicação (renderizado a todo momento). Dessa forma, utilizando Hooks de vue-router, podemos ter a informação de qual rota está sendo acessada neste momento da aplicação, e fazer o tratamento (token ou redirecionamento) da forma correta
+
+    // O Watch é uma feature do Vue que nos fornece uma alternativa para  "escutar" eventos na aplicação. Ele recebe dois parâmetros: Uma função que retorna o que queremos escutar e uma outra que será chamada quando houver mudança.
     watch(
       () => route.path,
       async () => {
@@ -28,7 +34,8 @@ export default {
             return;
           }
 
-          const { data } = await services.users.getMe();
+          const { data } = await services.user.getMe();
+          setCurrentUser(data);
           console.log('data: ', data);
         }
       }
