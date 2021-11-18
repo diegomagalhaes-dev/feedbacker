9429ca6c57dfa68a7d7f78e3d9230a6762ccdd8b
diegomagalhaes-dev <digengup@gmail.com>
2021-11-17 22:27:15 -0300

    :zap: Create copy icon

--- dashboard/src/components/Icon/index.vue
+++ dashboard/src/components/Icon/index.vue
@@ -1,12 +1,14 @@
 <template>
+  <!-- Utilizando o "component is" do vue, podemos determinar a forma de um component de forma "assíncrona". Pois, com ele, o que este component vai ser só é determinado quando instanciamos ele determinamos o valor de "is".  -->
   <component :is="name" v-bind="$props" />
 </template>
 
 <script>
 import Loading from './Loading.vue';
+import Copy from './Copy.vue';
 
 export default {
-  components: { Loading },
+  components: { Loading, Copy },
   props: {
     name: { type: String, required: true }
   }
